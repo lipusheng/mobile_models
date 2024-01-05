a89206825ecfc794f46c39a727807867f95445af
pusheng.li01 <pusheng.li01@liulishuo.com>
2024-01-05 10:20:32 +0800

    add colume big_brand for result; fix model contains " " to the parse error

--- phoneModel.py
+++ phoneModel.py
@@ -13,10 +13,10 @@ from git import Repo  # rely on gitpython
 class PhoneModel:
     def __init__(self):
         """获取手机品牌型号"""
-        # if origin == 0:  # 拉取仓库
-        #     repo = Repo.clone_from(repo_address, repo_path)
-        # else:  # 拉取最新数据
-        repo = Repo(repo_path)
+        if os.path.exists(repo_path) == 0:  # 拉取仓库
+            repo = Repo.clone_from(repo_address, repo_path)
+        else:  # 拉取最新数据
+            repo = Repo(repo_path)
         repo.remotes.origin.pull()
         self.new_commit = repo.head.commit.hexsha
         print("MobileModels latest commit: " + str(self.new_commit))
@@ -28,26 +28,33 @@ class PhoneModel:
         """读取原始数据"""
         with open(os.path.join(repo_path, 'brands', brand), 'rt') as f:
             brand_info = f.read()
-        brand_info_list = [item for item in brand_info.split('\n') if re.match('`.+: ', item) is not None]
+        brand_info_list = [item for item in brand_info.split('\n') if re.match('[`.+:|\\*.+:]', item) is not None]
 
-        model_df = pd.DataFrame(columns=['brand', 'model', 'area', 'brand_name', 'model_name'])
+        model_df = pd.DataFrame(columns=['brand', 'model', 'area', 'brand_name', 'model_name', 'big_brand'])
+        big_brand = 'UNKNOWN'
         for record in brand_info_list:
-            record_list = record.replace('`', '').split(':')
+            record_list = record.split(':')
             if record_list[1] == '**':
+                big_brand = record_list[0].replace('*', '')
+                if '] ' in big_brand:
+                    big_brand = big_brand.split('] ')[1]
+
+                if ' (`' in big_brand:
+                    big_brand = big_brand.split(' (`', 1 )[0]
                 continue
             model_str = record_list[0].replace(brand[:-3].split('_')[0].upper(), '').strip()
-            model_list = [x for x in model_str.split() if x not in ('SHARK', 'HUAWEI', 'Letv', 'Le', 'ONE')]
-            head = model_list[0][:3]
-            tail = model_list[0][-3:]
-            if all([x.startswith(head) or x.endswith(tail) or x.find('-') > 0 for x in model_list]):
-                for model in model_list:
-                    model_df.loc[len(model_df)] = (
-                    brand[:-3].split('_')[0], model, 'en' if brand.find('_en') > 0 else 'cn',
-                    brand_map.get(brand[:-3].split('_')[0], '其他'), record_list[1])
+            model_list = [x for x in model_str.split('` `') if x not in ('SHARK', 'HUAWEI', 'Letv', 'Le', 'ONE')]
+            # head = model_list[0][:3]
+            # tail = model_list[0][-3:]
+            # if all([x.startswith(head) or x.endswith(tail) or x.find('-') > 0 for x in model_list]):
+            for model in model_list:
+                model_df.loc[len(model_df)] = (
+                brand[:-3].split('_')[0], model.replace('`', ''), 'en' if brand.find('_en') > 0 else 'cn',
+                brand_map.get(brand[:-3].split('_')[0], '其他'), record_list[1].lstrip(), big_brand)
         return model_df
 
     def get_all(self):
-        brand_model = pd.DataFrame(columns=['brand', 'model', 'area', 'brand_name', 'model_name'])
+        brand_model = pd.DataFrame(columns=['brand', 'model', 'area', 'brand_name', 'model_name', 'big_brand'])
 
         for brand in self.brands:
             self.get_model(brand)
@@ -65,8 +72,8 @@ class PhoneModel:
 
 if __name__ == '__main__':
     # repo_path = os.path.join(os.path.expanduser('~'), 'MobileModels')
-    repo_path = './MobileModels/'
-    # repo_address = 'https://github.com/KHwang9883/MobileModels.git'
+    repo_path = './MobileModelss'
+    repo_address = 'https://github.com/lipusheng/MobileModels.git'
 
     brand_map = {'meizu': '魅族', 'smartisan': '锤子', 'vivo': 'VIVO', 'realme': '真我',
                  'xiaomi': '小米', 'apple': '苹果', 'oppo': 'OPPO', 'nokia': '诺基亚',
@@ -82,8 +89,8 @@ if __name__ == '__main__':
         last_commit = ''
 
     pm = PhoneModel()  # 初始 pm=PhoneModel(0),后续更新可不填
-    if pm.new_commit != last_commit:
-        pm.get_all()
-        pm.data_save()
-    else:
-        print("No update, skip.")
+    # if pm.new_commit != last_commit:
+    pm.get_all()
+    pm.data_save()
+    # else:
+    print("No update, skip.")
